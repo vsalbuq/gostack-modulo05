c45facf7d844912de17fc3e6d2b840a64df52491
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2019-07-20 19:38:10 -0300

    feat: adds prop-types validation for necessary data

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import api from '../../services/api';
 
 // import { Container } from './styles';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   state = {
     repository: {},
     issues: [],
@@ -15,7 +25,7 @@ export default class Repository extends Component {
 
     const repoName = decodeURIComponent(match.params.repository);
 
-    const [repository, issue] = await Promise.all([
+    const [repository, issues] = await Promise.all([
       api.get(`/repos/${repoName}`),
       api.get(`/repos/${repoName}/issues`, {
         params: {
